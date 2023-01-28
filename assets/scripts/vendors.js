dfa24b17e4c35585a024393f9dbd83f1a0c1b40f
Djole <79225724+djordjeerkicecka@users.noreply.github.com>
2023-01-28 16:16:18 +0100

    Scripts folder cleaned up, vendor file optimised, html adjusted

---
+++ assets/scripts/vendors.js
@@ -0,0 +1,32 @@
+// Game Windows
+const windowStart = document.getElementById('window--start');
+const windowGame = document.getElementById('window--game');
+
+// Start buttons 
+const startBtnEasy = document.getElementById('start--btn-easy');
+const startBtnMedium = document.getElementById('start--btn-medium');
+const startBtnHard = document.getElementById('start--btn-hard');
+
+// Modals
+const modalGameOver = document.getElementById('modal--game-over');
+const modalRegistration = document.getElementById('modal--register');
+
+// Modal Controls
+const modalBtnConfirm = document.getElementById('modal--btn-yes');
+const modalBtnDecline = document.getElementById('modal--btn-no');
+const modalBtnSubmit = document.getElementById('modal--btn-submit');
+
+// Modal Status 
+const modalStatusScore = document.getElementById('modal--status-score');
+const modalStatusTime = document.getElementById('modal--status-time');
+
+// Modal Form Components
+const modalForm = document.getElementById('modal--form');
+const modalInput = document.getElementById('modal--input')
+
+// Status Messages 
+const statusScore = document.getElementById('status--score');
+const statusTime = document.getElementById('status--time');
+
+// Game Window Playing Field
+const playingField = document.getElementsById('playing-field');
